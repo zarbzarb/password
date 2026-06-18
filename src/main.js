6ee28f8ba619037e0e1019238d3fd4bcf057a374
zarbzarb <326836190@qq.com>
2026-06-18 17:09:58 +0800

    fix: 注册 Vant 组件

--- src/main.js
+++ src/main.js
@@ -3,10 +3,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'vant/lib/index.css'
+import { 
+  Button, 
+  Icon, 
+  Search, 
+  FloatingBubble,
+  Dialog,
+  Toast,
+  Popup
+} from 'vant'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+
+app.use(Button)
+app.use(Icon)
+app.use(Search)
+app.use(FloatingBubble)
+app.use(Dialog)
+app.use(Toast)
+app.use(Popup)
+
 app.mount('#app')
